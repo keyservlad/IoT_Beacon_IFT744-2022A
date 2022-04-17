768960b2d376691bffa0c636571cb1a90bcc181c
Souhail Maraoui <mars3319@usherbrooke.ca>
2022-04-17 19:27:09 +0100

    Added post request with a new data model

---
+++ script.sh
@@ -0,0 +1,7 @@
+apt-get install -y vim git pip libglib2.0-dev 
+
+git clone https://depot.ami.usherbrooke.ca/IFT744/IoT_Beacon-IFT744-2022A.git
+
+
+pip install bluepy
+python IoT_Beacon-IFT744-2022A/Gateway/scanner.py
